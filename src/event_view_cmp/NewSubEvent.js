0145a062d29c8d33bd5fe9abf5785d047467b338
mikkokallio <mikko.m.kallio@gmail.com>
2018-08-31 10:20:57 +0300

    Dynamic dropdown for places works. Subevent submission works.

--- src/event_view_cmp/NewSubEvent.js
+++ src/event_view_cmp/NewSubEvent.js
@@ -2,14 +2,27 @@ import React from 'react';
 import axios from 'axios';
 
 class NewSubEvent extends React.Component {
-    state= { name: '', begin:'', end:'',
-        placeId:'', eventId:'1', type:'', workId:'1'};
+    state= { json:[], name: '', begin:'', ending:'',
+        placeId:'', eventId:'', type:'', workId:'1'};
+
+    componentDidMount()
+    {
+        this.load();
+    }
+
+    load = () => {
+        this.setState({isLoading: true});
+        axios.get('/api/places')
+            .then(response => {
+                const json = response.data;
+                this.setState({json});
+            });
+    };
 
     handleCreateClick= (event) => {
         //event.preventDefault();
-        axios.post('/api/subevents', { name:this.state.name, begin:this.state.begin, ending:this.state.end,
-        placeId:this.state.placeId, eventId:this.state.eventId, type:this.state.type, workId:this.state.workId})
-        // name, begin, end, placeid, eventid, type, workid
+        axios.post('/api/subevents', { name:this.state.name, begin:this.state.begin, ending:this.state.ending,
+        placeId:this.state.placeId, eventId:this.props.event, type:this.state.type, workId:this.state.workId})
             .then(res => {
                 this.props.callBack();
             });
@@ -37,7 +50,9 @@ class NewSubEvent extends React.Component {
                 <td><input type="text" placeholder="tyyppi" value={this.state.type} onChange={this.handleTypeChange}/></td>
                 <td><input type="datetime-local" placeholder="alkaa" value={this.state.begin} onChange={this.handleBeginChange}/></td>
                 <td><input type="datetime-local" placeholder="loppuu" value={this.state.ending} onChange={this.handleEndChange}/></td>
-                <td><input type="text" placeholder="sijainti" value={this.state.placeId} onChange={this.handlePlaceChange}/></td>
+                <td><select placeholder="sijainti" value={this.state.placeId} onChange={this.handlePlaceChange}>
+                    {this.state.json.map((data, index) => <option value={data.id} label={data.name} data={data}/>)}
+                </select></td>
                 <td><div className="circle" onClick={this.handleCreateClick.bind(this)}><span className="glyphicon glyphicon-plus"></span></div></td></tr>
         );
     }
