dddc18fca5a86cbebe67a7adcfc5f83c027917c7
pirjotuulia <pirjoleppanen@nic.fi>
2018-09-02 20:02:35 +0300

    teosten muokkaaminen toimii

--- src/works_cmp/EditWork.js
+++ src/works_cmp/EditWork.js
@@ -15,7 +15,7 @@ class EditWork extends React.Component {
             .then(response => {
                 const json = response.data;
                 this.setState(json);//täällä sisällä "roleList", joka sisältää teoksen roolit
-                let chosenList = this.state.roleList? this.state.roleList : [];
+                let chosenList = this.state.roleList ? this.state.roleList : [];
                 this.setState({ chosenRoles: chosenList });
                 // console.log("chosenRoles", this.state.chosenRoles)
                 // console.log(this.state.roleList[0].name);
@@ -39,15 +39,38 @@ class EditWork extends React.Component {
                 this.setState(this.state);
             });
     };
-    handleNameChange = (event) => {
-        this.setState({ name: event.target.value });
+
+    handleUpdateClick = (event) => {
+        axios.put('/api/works/'+this.state.id, { work:this.state.work, composer:this.state.composer, musicians:this.state.musicians,
+            durationInMinutes:this.state.durationInMinutes, instrumentation:this.state.instrumentation})
+            .then(res => {
+                // this.props.callBack();
+                this.load();
+ //               this.setState({work: '', composer: '', durationInMinutes: '', musicians: '', instrumentation: ''});
+            });
+    };
+    handleWorkNameChange = (event) => {
+        this.setState({ work: event.target.value });
+    };
+
+    handleComposerChange = (event) => {
+        this.setState({ composer: event.target.value });
+    };
+    handleDurChange = (event) => {
+        this.setState({ durationInMinutes: event.target.value });
+    };
+    handleMusChange = (event) => {
+        this.setState({ musicians: event.target.value });
+    };
+    handleInstChange = (event) => {
+        this.setState({ instrumentation: event.target.value });
     };
     handleRoleChange = (event) => {
         let newList = this.state.newRoles ? this.state.newRoles : [];//otetaan joko olemassaoleva roleList(tullut apista ja päivittyy lisättäessä) tai aloitetaan uusi;
         newList.push(event.target.value);//lisätään lisättävien roolien listalle valitun roolin numero
         let chosenList = this.state.chosenRoles ? this.state.chosenRoles : [];//tämä lista on näkymää varten
         chosenList.push(this.state.allRoles.find(role => role.id === Number(event.target.value)));//valittu rooli lisätään myös näkymään
-        this.setState({newRoles: newList, chosenRoles: chosenList});//päivitetään tilaan uudet listat
+        this.setState({ newRoles: newList, chosenRoles: chosenList });//päivitetään tilaan uudet listat
     };
 
     render() {
@@ -60,22 +83,28 @@ class EditWork extends React.Component {
                 </div>
                 <table className="boxx table-striped">
                     <thead>
-                        <tr><th colSpan={2}><span className="glyphicon glyphicon-music"></span><span> </span>{this.state.work}</th></tr>
+                        <tr><th colSpan={2}><span className="glyphicon glyphicon-music"></span><span> </span>{this.state.work}</th>
+                            <th><input type="text" placeholder="" value={this.state.work} onChange={this.handleWorkNameChange} /></th></tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td>Säveltäjä</td>
                             <td>{this.state.composer}</td>
+                            <td><input type="text" placeholder="" value={this.state.composer} onChange={this.handleComposerChange} /></td>
                         </tr>
                         <tr><td>Kesto</td>
                             <td>{this.state.durationInMinutes}</td>
+                            <td><input style={{ width: '20px' }} type="text" placeholder="" value={this.state.durationInMinutes} onChange={this.handleDurChange} /></td>
                         </tr>
                         <tr><td>Muusikot</td>
                             <td>{this.state.musicians}</td>
+                            <td><input style={{ width: '20px' }} type="text" placeholder="" value={this.state.musicians} onChange={this.handleMusChange} /></td>
                         </tr>
                         <tr><td>Instrumentaatio</td>
                             <td>{this.state.instrumentation}</td>
+                            <td><input type="text" placeholder="" value={this.state.instrumentation} onChange={this.handleInstChange} /></td>
                         </tr>
+                        <tr><td colSpan="2"><div className="circle" onClick={this.handleUpdateClick.bind(this)}><span className="glyphicon glyphicon-plus"></span></div> Päivitä tiedot </td></tr>
                     </tbody>
                 </table>
                 {this.state.chosenRoles ? this.state.chosenRoles.map((line, index) => <Role key={index} data={line} />) : 'LOADING, oh my!'}
@@ -84,15 +113,15 @@ class EditWork extends React.Component {
                 </select></div>
 
                 <tr>
-                    <td><span className="glyphicon glyphicon-music"></span>
+                    {/* <td><span className="glyphicon glyphicon-music"></span>
                         <input type="text" placeholder="nimi" value={this.state.name} onChange={this.handleNameChange} />
-                    </td>
+                    </td> */}
                     <td colSpan="2">
                         <div className="circle" onClick={this.handleCreateClick.bind(this)}><span
-                            className="glyphicon glyphicon-plus"></span></div>
+                            className="glyphicon glyphicon-plus"></span></div>Päivitä teoksen roolit 
                     </td>
                 </tr>
-            </div>
+            </div >
 
         );
     }
